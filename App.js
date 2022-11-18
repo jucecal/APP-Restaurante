722ce082a0768dd1de683586d63665f4e1f683b9
Julio Caballero <jucecal24@hotmail.com>
2022-11-17 23:10:17 -0600

    Update App

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bienvenido a Come Rico</Text>
+      <Text>¡Bienvenido a Come Rico!</Text>
       <StatusBar style="auto" />
     </View>
   );
