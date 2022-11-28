cb9864ef942dc3f63c8ccd42c51de6567a4175af
jall0101 <92348560+jall0101@users.noreply.github.com>
2022-11-27 18:27:27 -0600

    ..

--- App.js
+++ App.js
@@ -8,36 +8,27 @@ import EditarProveedor from './src/Pantallas/EditarProveedor';
 import Cliente from './src/Pantallas/Cliente';
 import Colaborador from './src/Pantallas/Colaborador';
 import EditarCliente from './src/Pantallas/EditarCliente';
-//<<<<<<< HEAD
 import Inventario from './src/Pantallas/Inventario';
-//=======
 import EditarColaborador from './src/Pantallas/EditarColaborador';
 import GuardarColaborador from './src/Pantallas/GuardarColaborador';
 import GuardarCliente from './src/Pantallas/GuardarCliente';
 
-//>>>>>>> 9e5701f54b1ea7f7a3a8957900bcd90a87ee2268
 
 export default function App() {
   return (
-    //<Login></Login>
+    <Login></Login>
     //<Menu></Menu>
     //<DetalleMenu></DetalleMenu>
     //<Carrito></Carrito>
     //<Proveedor></Proveedor>
     //<EditarProveedor></EditarProveedor>
-//<<<<<<< HEAD
-//=======
-    //---------------------
-//>>>>>>> 9e5701f54b1ea7f7a3a8957900bcd90a87ee2268
     //<Cliente></Cliente>
     //<EditarCliente></EditarCliente>
     //<Colaborador></Colaborador>
     //<EditarColaborador></EditarColaborador>
-//<<<<<<< HEAD
-    <Inventario></Inventario>
-//=======
+    //<GuardarColaborador></GuardarColaborador>
+    //<Inventario></Inventario>
     //<GuardarColaborador></GuardarColaborador>
     //<GuardarCliente></GuardarCliente>
-//>>>>>>> 9e5701f54b1ea7f7a3a8957900bcd90a87ee2268
   );
 }
