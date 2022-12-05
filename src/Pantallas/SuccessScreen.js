4f16d4932b5b2c75d07bf395576f24e777250139
jall0101 <92348560+jall0101@users.noreply.github.com>
2022-12-05 02:24:43 -0600

    MENÚ

--- src/Pantallas/SuccessScreen.js
+++ src/Pantallas/SuccessScreen.js
@@ -16,7 +16,7 @@ function SuccessScreen() {
                 <Text style={styles.title}>Felicidades!!!</Text>
                 <Text style={styles.text}>Tu orden ha sido ejecutada exitosamente!</Text>
                 <View style={styles.buttons}>
-                    <AppButton onPress={() => navigation.navigate('HomeScreen')} title="Ir a Inicio" color="primary" />
+                    <AppButton onPress={() => navigation.navigate('Menú')} title="Ir a Inicio" color="primary" />
                 </View>
             </View>
         </Screen>
