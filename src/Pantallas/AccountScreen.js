15ee8ac2ee9ced8270cdaa3bdc68f0c31a8ca6fb
Julio Caballero <jucecal24@hotmail.com>
2022-12-05 01:21:25 -0600

    Cambios

--- src/Pantallas/AccountScreen.js
+++ src/Pantallas/AccountScreen.js
@@ -40,56 +40,31 @@ const AccountScreen = () => {
                             <Text style={tailwind`mt-4 text-3xl font-bold text-red-500`}>{usuario.nombre}</Text>
                             <Text style={tailwind`text-lg text-indigo-900`}>{usuario.correo}</Text>
                         </View>
-
                         <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
                             <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Administrar Opciones</Text>
                             <SavedPlaces
-                                title="Clientes"
-                                text="Agregar, editar y eliminar clientes."
-                                Icon={() => <AntDesign name="user" size={24} color="black" />}
-                            />
-                            <SavedPlaces
-                                title="Empleados"
-                                text="Agregar, editar y eliminar empleados."
-                                Icon={() => <AntDesign name="idcard" size={24} color="black" />}
-                            />
-
-                            <SavedPlaces
-                                title="Proveedores"
-                                text="Agregar, editar y eliminar proveedores."
-                                Icon={() => <Feather name="truck" size={24} color="black" />}
-                            />
-                            <SavedPlaces
-                                title="Inventario"
-                                text="Ver Inventario."
-                                Icon={() => <Feather name="list" size={24} color="black" />}
+                                title="Términos y condiciones"
                             />
                         </View>
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Administrar Opciones</Text>
-                <SavedPlaces
-                    title="Términos y condiciones"
-                />
-            </View>
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <SavedPlaces
-                    title="Privacidad"
-                />
-            </View>
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <SavedPlaces
+                                title="Privacidad"
+                            />
+                        </View>
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <SavedPlaces
-                    title="Ayuda"
-                />
-            </View>
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <SavedPlaces
+                                title="Ayuda"
+                            />
+                        </View>
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
-                <TouchableOpacity>
-                    <Text style={tailwind`text-red-500 mt-3 text-sm`} onPress={cerrarSesion}>Cerrar Sesión</Text>
-                </TouchableOpacity>
-            </View>
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
+                            <TouchableOpacity>
+                                <Text style={tailwind`text-red-500 mt-3 text-sm`} onPress={cerrarSesion}>Cerrar Sesión</Text>
+                            </TouchableOpacity>
+                        </View>
 
                         <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
                             <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
@@ -106,7 +81,7 @@ const AccountScreen = () => {
 
 export default AccountScreen;
 
-const SavedPlaces = ({ title}) => (
+const SavedPlaces = ({ title }) => (
     <TouchableOpacity style={tailwind`flex-row items-center my-3`}>
         <View style={tailwind`ml-5`}>
             <Text style={tailwind`text-gray-800`}>{title}</Text>
