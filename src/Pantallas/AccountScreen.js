4c797262211a7a1f43f4ca103015537afa66ed0b
Julio Caballero <jucecal24@hotmail.com>
2022-12-05 01:47:38 -0600

    Revert "Imagen Cliente"
    This reverts commit 64f1b3a42f6451ed82efac013b63214e10e224a8.

--- src/Pantallas/AccountScreen.js
+++ src/Pantallas/AccountScreen.js
@@ -3,78 +3,63 @@ import { View, Image, Text, TouchableOpacity, Icon } from 'react-native';
 import Screen from '../Componentes/Screen'
 
 import tailwind from 'tailwind-react-native-classnames';
-import UsuarioContext from '../contexto/UsuarioContext';
 import AppHead from '../Componentes/AppHead';
 import { AntDesign } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import UsuarioContext from '../contexto/UsuarioContext';
-import { urlImagenesUsuariosEM, urlImagenesUsuariosCL } from '../configuracion/Urls';
 
 const AccountScreen = () => {
-    const { usuario, setCerrarSesion } = useContext(UsuarioContext);
-    const [espera, setEspera] = useState(false);
+    const { setCerrarSesion } = useContext(UsuarioContext);
     const cerrarSesion = async () => {
         await setCerrarSesion();
     };
 
     return (
         <Screen style={tailwind`flex-1 bg-white`}>
-            {
-                espera ? (
-                    <Cargando texto="Estableciendo conexion con la API"></Cargando>
-                ) : (
-                    <>
-                        <AppHead title={`Cuenta`}
-                            icon="settings-outline"
-                        />
+            <AppHead title={`Cuenta`}
+                icon="settings-outline"
+            />
 
-                        <View style={tailwind`justify-center items-center`}>
-                            <View style={tailwind`rounded-full overflow-hidden w-48 h-48 mt-4`}>
-                                <View style={styles.contenedorImagen}>
-                                    <Image
-                                        style={styles.imagen}
-                                        source={{ uri: urlImagenesUsuariosCL + usuario.imagen }}
-                                    />
-                                </View>
-                            </View>
-                            <Text style={tailwind`mt-4 text-3xl font-bold text-red-500`}>{usuario.nombre}</Text>
-                            <Text style={tailwind`text-lg text-indigo-900`}>{usuario.correo}</Text>
-                        </View>
+            <View style={tailwind`justify-center items-center`}>
+                <View style={tailwind`rounded-full overflow-hidden w-48 h-48 mt-4`}>
+                    <Image source={require('../../assets/avatar.gif')} style={tailwind`w-48 h-48`} />
+                </View>
+                <Text style={tailwind`mt-4 text-3xl font-bold text-red-500`}>Lewa</Text>
+                <Text style={tailwind`text-lg text-indigo-900`}>Lewa@gmail.com</Text>
+            </View>
 
-                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                            <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Administrar Opciones</Text>
-                            <SavedPlaces
-                                title="Clientes"
-                                text="Agregar, editar y eliminar clientes."
-                                Icon={() => <AntDesign name="user" size={24} color="black" />}
-                            />
-                            <SavedPlaces
-                                title="Empleados"
-                                text="Agregar, editar y eliminar empleados."
-                                Icon={() => <AntDesign name="idcard" size={24} color="black" />}
-                            />
+            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Administrar Opciones</Text>
+                <SavedPlaces
+                    title="Clientes"
+                    text="Agregar, editar y eliminar clientes."
+                    Icon={() => <AntDesign name="user" size={24} color="black" />}
+                />
+                <SavedPlaces
+                    title="Empleados"
+                    text="Agregar, editar y eliminar empleados."
+                    Icon={() => <AntDesign name="idcard" size={24} color="black" />}
+                />
 
-                            <SavedPlaces
-                                title="Proveedores"
-                                text="Agregar, editar y eliminar proveedores."
-                                Icon={() => <Feather name="truck" size={24} color="black" />}
-                            />
-                            <SavedPlaces
-                                title="Inventario"
-                                text="Ver Inventario."
-                                Icon={() => <Feather name="list" size={24} color="black" />}
-                            />
-                        </View>
+                <SavedPlaces
+                    title="Proveedores"
+                    text="Agregar, editar y eliminar proveedores."
+                    Icon={() => <Feather name="truck" size={24} color="black" />}
+                />
+                <SavedPlaces
+                    title="Inventario"
+                    text="Ver Inventario."
+                    Icon={() => <Feather name="list" size={24} color="black" />}
+                />
+            </View>
 
-                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                            <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
-                            <TouchableOpacity>
-                                <Text style={tailwind`text-red-500 mt-3 text-sm`} onPress={cerrarSesion}>Cerrar Sesión</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </>
-                )
-            }
+            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
+                <TouchableOpacity>
+                    <Text style={tailwind`text-red-500 mt-3 text-sm`} onPress={cerrarSesion}>Cerrar Sesión</Text>
+                </TouchableOpacity>
+            </View>
+
         </Screen>
     );
 }
