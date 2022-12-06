53c098a03b8f963ec4fe7c538cb2a1db3e10e1b4
jall0101 <92348560+jall0101@users.noreply.github.com>
2022-12-06 17:09:13 -0600

    Pnatalla de Inicio administradores

--- src/Pantallas/GroceryScreen.js
+++ src/Pantallas/GroceryScreen.js
@@ -1,21 +1,91 @@
 import React from 'react';
-import { View, ImageBackground, StyleSheet, ScrollView, Text, TouchableOpacity, Image, TextInput } from 'react-native';
+import { View, ImageBackground, StyleSheet, ScrollView, Text, TouchableOpacity, Image, TextInput, style } from 'react-native';
 import Screen from '../Componentes/Screen';
+import logo01 from '../../assets/nombre.png'
 import tailwind from 'tailwind-react-native-classnames';
-import colors from '../consts/colors2';
+import colors from '../consts/colors';
+//<Image source={require('../../assets/nombre.png')} style={tailwind`w-72 h-72`} />
 
 const GroceryScreen = () => {
     return (
         <Screen style={tailwind`flex-1`}>
-            <View style={tailwind` justify-center items-center`}>
-                <Image source={require('../../assets/nombre.png')} style={tailwind`w-72 h-72`} />
-                <Image source={require('../../assets/pizza.gif')} style={tailwind`w-72 h-72`} />
-                <View style={tailwind`w-3/4`}>
-                    <Text style={tailwind`text-lg text-center`}>Bienvenido al sistema de gestión administrativo</Text>
+
+            <View style={styles.contenedorImagen}>
+                <ImageBackground
+                    source={logo01}
+                    style={styles.imagenFondo}
+                >
+                </ImageBackground>
+
+                <View style={tailwind` justify-center items-center`}>
+                    <View style={styles.Gif}>
+                        <Image source={require('../../assets/pizza.gif')} style={tailwind`w-72 h-72`} />
+                    </View>
+
+                    <View style={styles.ContenedorTexto}>
+                        <Text
+                            style={styles.TextoTitulo}
+                        >
+                            ¡¡Bienvenido!!
+                        </Text>
+
+                        <Text
+                            style={styles.Texto}
+                        >
+                            Sistema de Gestión Administrativo.
+                        </Text>
+                    </View>
                 </View>
-            </View>
+
+            </View>    
+
         </Screen>
     );
 }
+
+
+
+const styles = StyleSheet.create({
+    input: {
+        borderColor: colors.medium,
+        borderWidth: 1,
+        marginBottom: 10,
+    },
+    contenedorImagen: {
+        backgroundColor: '#f6f3e1',
+        alignItems: 'center',
+        height: 970,
+
+    },
+    Gif: {
+        paddingTop: 100,
+        backgroundColor: '#f6f3e1',
+        alignContent:'center',
+        height: 350,
+
+    },
+    imagenFondo: {
+        width: 190,
+        height: 120,
+        backgroundColor: '#f6f3e1',
+    },
+    ContenedorTexto: {
+        alignItems:'center',
+        width: 400,
+        height: 320,
+    },
+    TextoTitulo: {
+        color: colors.primary,
+        fontSize: 30,
+        justifyContent:'center',
+    },
+    Texto: {
+        color: colors.dark,
+        fontSize: 17,
+        justifyContent:'center',
+    },
+
+})
+
 
 export default GroceryScreen;
