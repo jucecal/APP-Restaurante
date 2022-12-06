ec587ad3446a031605d1235be669d0e3861f62fc
Julio Caballero <jucecal24@hotmail.com>
2022-12-06 04:19:15 -0600

    Guardar (Empleados, Proveedores)

--- src/Pantallas/GuardarEmpleado.js
+++ src/Pantallas/GuardarEmpleado.js
@@ -1,10 +1,93 @@
 import React from 'react';
 import { TouchableOpacity, View, Text, ScrollView, TextInput } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import AppForm from "../Componentes/forms/AppForm";
 import COLORS from '../consts/colors';
+import * as yup from "yup";
+import AppFormFeilds from "../Componentes/forms/AppFormFeilds";
 import EstilosEditar from '../Componentes/EstilosEditar';
+import Axios from '../Componentes/Axios';
+import AppSubmitButton from '../Componentes/forms/AppSubmitButton';
 
-const GuardarEmpleado = ({navigation}) => {
+const PostValidationSchema = yup.object().shape({
+    nombre: yup
+        .string()
+        .required("El nombre es requerido")
+        .min(4, ({ min }) => `El nombre debe de tener un minimo de ${min} caracteres`),
+    apellido: yup
+        .string()
+        .min(4, ({ min }) => `El apellido debe de tener un minimo de ${min} caracteres`)
+        .required("El apellido es requerido"),
+    telefono: yup
+        .number()
+        .min(8, ({ min }) => `El telefono debe de tener un minimo de ${min} caracteres`)
+        .integer()
+        .positive()
+        .required("El telefono es requerido"),
+    direccion: yup
+        .string(),
+    usuarioID: yup
+        .number()
+        .integer()
+        .positive()
+        .required("El Id del usuario es requerido"),
+    sucursalID: yup
+        .number()
+        .integer()
+        .positive()
+        .required("El Id de la sucursal es requerido"),
+    cargoID: yup
+        .number()
+        .integer()
+        .positive()
+        .required("El Id del cargo es requerido"),
+});
+
+const GuardarEmpleado = ({ navigation }) => {
+
+    const guardar = async ({ nombre, apellido, telefono, direccion, usuarioID, sucursalID, cargoID }) => {
+        try {
+            var textoMensaje = "";
+
+            await Axios.post('/empleados/guardar', {
+
+                nombre: nombre,
+                apellido: apellido,
+                telefono: telefono,
+                direccion: direccion,
+                UsuarioId: usuarioID,
+                SucursalId: sucursalID,
+                CargoId: cargoID
+            })
+                .then(async (data) => {
+                    const json = data.data;
+                    if (json.errores.length == 0) {
+                        console.log(data.data);
+                    }
+                    else {
+                        textoMensaje = '';
+                        json.errores.forEach(element => {
+                            textoMensaje += element.mensaje + '. ';
+                        });
+                    }
+                })
+                .catch((error) => {
+                    textoMensaje = error;
+                });
+        } catch (error) {
+            textoMensaje = error;
+            console.log(error);
+        }
+
+        console.log(nombre);
+        console.log(apellido);
+        console.log(telefono);
+        console.log(direccion);
+        console.log(usuarioID);
+        console.log(sucursalID);
+        console.log(cargoID);
+
+    }
 
     return (
         <View style={{ backgroundColor: COLORS.white }}>
@@ -13,50 +96,94 @@ const GuardarEmpleado = ({navigation}) => {
                 <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 20 }}>Guardar Empleado</Text>
             </View>
             <ScrollView style={{ marginTop: 0 }}>
-                <View>
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Nombre</Text>
-                        <TextInput style={EstilosEditar.inputs}></TextInput>
-                    </View>
+                <View style={styles.form}>
+                    <AppForm
+                        initialValues={{ nombre: "", apellido: "", telefono: 0, direccion: "", usuarioID: 0, cargoID: 0, sucursalID: 0 }}
+                        validationSchema={PostValidationSchema}
+                        onSubmit={(values) => guardar(values)}
+                    >
+                        <AppFormFeilds
+                            name="nombre"
+                            placeholder="Nombre"
+                        />
 
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Apellido</Text>
-                        <TextInput style={EstilosEditar.inputs}></TextInput>
-                    </View>
+                        <AppFormFeilds
+                            name="apellido"
+                            placeholder="Apellido"
+                        />
 
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Teléfono</Text>
-                        <TextInput style={EstilosEditar.inputs}></TextInput>
-                    </View>
+                        <AppFormFeilds
+                            name="telefono"
+                            placeholder="Telefono"
+                        />
 
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Fecha de Nacimiento</Text>
-                        <TextInput style={EstilosEditar.inputs}></TextInput>
-                    </View>
+                        <AppFormFeilds
+                            name="direccion"
+                            placeholder="Dirección"
+                        />
 
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Dirección</Text>
-                        <TextInput style={EstilosEditar.inputs}></TextInput>
-                    </View>
+                        <AppFormFeilds
+                            name="usuarioID"
+                            placeholder="ID Usuario"
+                        />
+
+                        <AppFormFeilds
+                            name="sucursalID"
+                            placeholder="ID Sucursal"
+                        />
+
+                        <AppFormFeilds
+                            name="cargoID"
+                            placeholder="ID Cargo"
+                        />
+                        <AppSubmitButton title="Ingresar" />
+                    </AppForm>
                 </View>
-
-                <View style={{ marginHorizontal: 30 }}>
-                    <TouchableOpacity activeOpacity={0.8} >
-                        <View style={EstilosEditar.btnContainer}>
-                            <Text style={EstilosEditar.title}>GUARDAR</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={EstilosEditar.contenedorBorrado}>
-                    <Text style={EstilosEditar.etiquetaCreacion}>Creado el 05/12/2022</Text>
-                </View>
-
             </ScrollView>
-
         </View>
-
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.white,
+        justifyContent: 'center'
+    },
+    wrapper: {
+        paddingHorizontal: 20,
+    },
+    logo: {
+        height: 280,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 10,
+    },
+    wellcomeTo: {
+        fontSize: 23,
+        fontWeight: "700",
+        color: COLORS.dark,
+        marginTop: 10,
+        textAlign: "center",
+    },
+    brand: {
+        fontSize: 23,
+        color: COLORS.primary,
+        textAlign: "center",
+        fontWeight: "500",
+    },
+    form: {
+        marginTop: 10,
+    },
+    join: {
+        marginTop: 16,
+        textAlign: "center",
+        color: COLORS.dark,
+    },
+    or: {
+        color: COLORS.grey,
+        textAlign: "center",
+        marginVertical: 20,
+    },
+});
 
 export default GuardarEmpleado;
--- src/Pantallas/GuardarProveedor.js
+++ src/Pantallas/GuardarProveedor.js
@@ -1,14 +1,70 @@
 
 import { TouchableOpacity, View, Text, ScrollView, Image, Alert, TextInput } from 'react-native';
 import NumericInput from 'react-native-numeric-input'
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import AppForm from "../Componentes/forms/AppForm";
 import COLORS from '../consts/colors';
-import EstilosEditar from '../Componentes/EstilosEditar';
+import * as yup from "yup";
+import AppFormFeilds from "../Componentes/forms/AppFormFeilds";
+import Axios from '../Componentes/Axios';
+import AppSubmitButton from '../Componentes/forms/AppSubmitButton';
 //import DatePicker from 'react-DatePicker';
 import ComboBox from 'react-native-combobox';
 import React, { useState, useEffect, useContext } from "react";
 
-const EditarReservacion = () => {
+const PostValidationSchema = yup.object().shape({
+    proveedor: yup
+        .string()
+        .required("El nombre es requerido")
+        .min(4, ({ min }) => `El nombre del proveedor debe de tener un minimo de ${min} caracteres`),
+    nombreContacto: yup
+        .string()
+        .min(4, ({ min }) => `El contacto debe de tener un minimo de ${min} caracteres`)
+        .required("El contacto es requerido"),
+    telefono: yup
+        .number()
+        .min(8, ({ min }) => `El telefono debe de tener un minimo de ${min} caracteres`)
+        .integer()
+        .positive()
+        .required("El telefono es requerido"),
+});
+
+const GuardarProveedor = () => {
+
+    const guardar = async ({ proveedor, nombreContacto, telefono }) => {
+        try {
+            var textoMensaje = "";
+
+            await Axios.post('/proveedor/guardar', {
+
+                proveedor: proveedor,
+                nombreContacto: nombreContacto,
+                telefono: telefono
+            })
+                .then(async (data) => {
+                    const json = data.data;
+                    if (json.errores.length == 0) {
+                        console.log(data.data);
+                    }
+                    else {
+                        textoMensaje = '';
+                        json.errores.forEach(element => {
+                            textoMensaje += element.mensaje + '. ';
+                        });
+                    }
+                })
+                .catch((error) => {
+                    textoMensaje = error;
+                });
+        } catch (error) {
+            textoMensaje = error;
+            console.log(error);
+        }
+
+        console.log(proveedor);
+        console.log(nombreContacto);
+        console.log(telefono);
+
+    }
 
     return (
         <View style={{ backgroundColor: COLORS.white }}>
@@ -17,81 +73,74 @@ const EditarReservacion = () => {
                 <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 20 }}>Reservaciones</Text>
             </View>
             <ScrollView showsVerticalScrollIndicator={false} style={{ marginTop: 30 }}>
-                <View>
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Fecha</Text>
-                        <Text>selected: {date.toLocaleString()}</Text>
-                        {show && (<DateTimePicker testID="dateTimePicker" value={date}
-                            mode={mode} is24Hour={true} onChange={onChange} />)}
-                    </View>
+                <View style={styles.form}>
+                    <AppForm
+                        initialValues={{ proveedor: "", nombreContacto: "", telefono: 0 }}
+                        validationSchema={PostValidationSchema}
+                        onSubmit={(values) => guardar(values)}
+                    >
+                        <AppFormFeilds
+                            name="proveedor"
+                            placeholder="Proveedor"
+                        />
 
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>fecha</Text>
-                        <TextInput style={EstilosEditar.inputs}>El bicho</TextInput>
-                        <Text>selected: {date.toLocaleString()}</Text>
-                             {show && (
-                            {/*<DatePicker
-                            testID="dateTimePicker"
-                            value={date}
-                            mode={mode}
-                            is24Hour={true}
-                         onChange={onChange}
-                             />*/}
-                            )}
-                    </View>
+                        <AppFormFeilds
+                            name="nombreContacto"
+                            placeholder="Nombre de Contacto"
+                        />
 
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>hora</Text>
-                        <TextInput style={EstilosEditar.inputs}>2PM</TextInput>
-                    </View>
-
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Cliente </Text>
-                        <View style={{ flex: 1, paddingVertical: 80, paddingHorizontal: 40, justifyContent: 'space-between' }}>
-                         <ComboBox
-                        values={values}
-                     onValueSelect={setSelectedValue}
-                     />
-                     <Text>selected value:          {values[selectedValue]}</Text>
-                     </View>
-                    </View>
-
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Mesa</Text>
-                        <View style={{ flex: 1, paddingVertical: 80, paddingHorizontal: 40, justifyContent: 'space-between' }}><ComboBox values={values}
-                        onValueSelect={setSelectedValue}
-                         />
-                           <Text>selected value:          {values[selectedValue]}</Text>
-                      </View>
-                    </View>
-                    <View style={EstilosEditar.contenedorContenido}>
-                        <Text style={EstilosEditar.etiqueta}>Sucursal</Text>
-                        <View style={{ flex: 1, paddingVertical: 80, paddingHorizontal: 40, justifyContent: 'space-between' }}> <ComboBox values={values}
-                         onValueSelect={setSelectedValue}
-                         />
-                <Text>selected value:          {values[selectedValue]}</Text>
-                    </View>
-                    </View>
-                </View>
-
-                <View style={{ marginHorizontal: 30 }}>
-                    <TouchableOpacity activeOpacity={0.8} >
-                        <View style={EstilosEditar.btnContainer}>
-                            <Text style={EstilosEditar.title}>GUARDAR</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <View style={EstilosEditar.contenedorBorrado}>
-                    <Text style={EstilosEditar.etiquetaCreacion}>Creado el 24/11/2022</Text>
-                    <TouchableOpacity style={EstilosEditar.botonBorrado}>
-                        <AntDesign name='delete' size={15} color={COLORS.white}></AntDesign>
-                    </TouchableOpacity>
+                        <AppFormFeilds
+                            name="telefono"
+                            placeholder="Telefono"
+                        />
+                        <AppSubmitButton title="Ingresar" />
+                    </AppForm>
                 </View>
             </ScrollView>
         </View>
     );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.white,
+        justifyContent: 'center'
+    },
+    wrapper: {
+        paddingHorizontal: 20,
+    },
+    logo: {
+        height: 280,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 10,
+    },
+    wellcomeTo: {
+        fontSize: 23,
+        fontWeight: "700",
+        color: COLORS.dark,
+        marginTop: 10,
+        textAlign: "center",
+    },
+    brand: {
+        fontSize: 23,
+        color: COLORS.primary,
+        textAlign: "center",
+        fontWeight: "500",
+    },
+    form: {
+        marginTop: 10,
+    },
+    join: {
+        marginTop: 16,
+        textAlign: "center",
+        color: COLORS.dark,
+    },
+    or: {
+        color: COLORS.grey,
+        textAlign: "center",
+        marginVertical: 20,
+    },
+});
 
-
-export default EditarReservacion;
+export default GuardarProveedor;
