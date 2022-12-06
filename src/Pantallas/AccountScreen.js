f299671bad7c2daf483e06ee8a7cb1f683f5380a
smilingD13 <agarciagomez13@gmail.com>
2022-12-05 23:10:10 -0600

    pantalla de perfil

--- src/Pantallas/AccountScreen.js
+++ src/Pantallas/AccountScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
-import { View, Image, Text, TouchableOpacity, Icon, StyleSheet } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Icon, StyleSheet, ScrollView } from 'react-native';
 import Screen from '../Componentes/Screen'
-
+import Cargando from '../Componentes/Cargando';
 import tailwind from 'tailwind-react-native-classnames';
 import AppHead from '../Componentes/AppHead';
 import { AntDesign } from '@expo/vector-icons';
@@ -10,59 +10,72 @@ import UsuarioContext from './../contexto/UsuarioContext';
 import { urlImagenesUsuariosEM, urlImagenesUsuariosCL } from '../configuracion/Urls';
 
 const AccountScreen = () => {
-    const { setCerrarSesion } = useContext(UsuarioContext);
+    const { usuario, setCerrarSesion } = useContext(UsuarioContext);
+    const [espera, setEspera] = useState(false);
     const cerrarSesion = async () => {
         await setCerrarSesion();
     };
 
     return (
         <Screen style={tailwind`flex-1 bg-white`}>
-            <AppHead title={`Cuenta`}
-                icon="settings-outline"
-            />
-
-            <View style={tailwind`justify-center items-center`}>
-                <View style={tailwind`rounded-full overflow-hidden w-48 h-48 mt-4`}>
-                    <Image source={require('../../assets/avatar.gif')} style={tailwind`w-48 h-48`} />
-                </View>
-                <Text style={tailwind`mt-4 text-3xl font-bold text-red-500`}>Lewa</Text>
-                <Text style={tailwind`text-lg text-indigo-900`}>Lewa@gmail.com</Text>
-            </View>
-
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Administrar Opciones</Text>
-                <SavedPlaces
-                    text="Términos y privacidad"
-                />
-            </View>
+            {
+                espera ? (
+                    <Cargando texto="Estableciendo conexion con la API"></Cargando>
+                ) : (
+                    <>
+                        <AppHead title={`Cuenta`}
+                            icon="settings-outline"
+                        />
 
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <SavedPlaces
-                    text="Política de privacidad y seguridad"
-                />
-            </View>
+                        <View style={tailwind`justify-center items-center`}>
+                            <View style={tailwind`rounded-full overflow-hidden w-48 h-48 mt-4`}>
+                                <Image
+                                    style={styles.imagen}
+                                    source={{ uri: urlImagenesUsuariosCL + usuario.imagen }}
+                                />
+                            </View>
+                            <Text style={tailwind`mt-4 text-3xl font-bold text-red-500`}>{usuario.nombre}</Text>
+                            <Text style={tailwind`text-lg text-indigo-900`}>{usuario.correo}</Text>
+                        </View>
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <SavedPlaces
-                    text="Políticas de reembolso"
-                />
-            </View>
+                        <ScrollView>
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <Text style={tailwind`text-gray-800 mt-2 text-lg mb-2`}>Administrar Opciones</Text>
+                            <SavedPlaces
+                                text="Términos y privacidad"
+                            />
+                        </View>
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <SavedPlaces
-                    text="Ayuda"
-                />
-            </View>
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <SavedPlaces
+                                text="Política de privacidad y seguridad"
+                            />
+                        </View>
 
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <SavedPlaces
+                                text="Políticas de reembolso"
+                            />
+                        </View>
 
-            <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
-                <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
-                <TouchableOpacity>
-                    <Text style={tailwind`text-red-500 mt-3 text-sm`} onPress={cerrarSesion}>Cerrar Sesión</Text>
-                </TouchableOpacity>
-            </View>
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <SavedPlaces
+                                text="Ayuda"
+                            />
+                        </View>
 
+                        <View style={tailwind`mx-4 border-t border-t-2 mt-5 border-gray-100`}>
+                            <Text style={tailwind`text-gray-800 mt-2 text-lg`}>Otras Opciones</Text>
+                            <TouchableOpacity>
+                                <Text style={tailwind`text-red-500 my-8 mt-3 text-sm`} onPress={cerrarSesion}>Cerrar Sesión</Text>
+                            </TouchableOpacity>
+                        </View>
+                        </ScrollView>
+                        
+                    </>
+                )
+            }
         </Screen>
     );
 }
